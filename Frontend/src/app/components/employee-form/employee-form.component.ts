631b4cb8f0f040630caa86c096b6133e5a111888
Abdelhamid-Mouloud <abdelhamidmouloudi2003@gmail.com>
2024-11-25 10:36:56 +0100

    frontend structure and implementation

---
+++ Frontend/src/app/components/employee-form/employee-form.component.ts
@@ -0,0 +1,41 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { EmployeeService } from '../../services/employee.service';
+
+@Component({
+  selector: 'app-employee-form',
+  templateUrl: './employee-form.component.html',
+  styleUrls: ['./employee-form.component.css'],
+})
+export class EmployeeFormComponent implements OnInit {
+  employee: any = {};
+  isEditMode: boolean = false;
+
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router,
+    private employeeService: EmployeeService
+  ) {}
+
+  ngOnInit(): void {
+    const id = this.route.snapshot.paramMap.get('id');
+    if (id) {
+      this.isEditMode = true;
+      this.employeeService.getEmployeeById(id).subscribe((data) => {
+        this.employee = data;
+      });
+    }
+  }
+
+  onSubmit(): void {
+    if (this.isEditMode) {
+      this.employeeService.updateEmployee(this.employee.id, this.employee).subscribe(() => {
+        this.router.navigate(['/employees']);
+      });
+    } else {
+      this.employeeService.addEmployee(this.employee).subscribe(() => {
+        this.router.navigate(['/employees']);
+      });
+    }
+  }
+}
